1c1f772d3837c2830d2bf38c250f46ebfd94ad85
AbdullahFasial <xqabdullhqx@gmail.com>
2024-12-06 08:54:03 +0300

    Update QueueStackTester.java

--- src/main/java/com/example/project/QueueStackTester.java
+++ src/main/java/com/example/project/QueueStackTester.java
@@ -1,31 +1,47 @@
 package com.example.project;
 
 public class QueueStackTester {
-    public static <T> void split(Queue<T> q, Queue<T> oq, Queue<T> eq)
-    {
-        throw new UnsupportedOperationException("Not supported yet.");
-        // Write the recursive static method split that splits a queue of n elements into two
-        // queues. The elements with odd orders (i.e. 1st, 3rd, 5th ...) should be put in the
-        // first queue and elements with even orders (i.e. 2nd, 4th, 6th ...) should be put in
-        // the second queue. The original queue should remain unchanged at the end of the
-        // method.
-        // Example. Given the queue (A, B, C, D, E), split results in oq (A, C, E), and eq(B, D).
-    }
-    public static <T> void remove(LinkedPQ<T> pq, int p)
-    {
-        throw new UnsupportedOperationException("Not supported yet.");
-        // Write a static method remove that removes every element in the priority queue
-        // having priority less than p.
-        // Example. Given pq: [A, 10], [D, 8], [B, 5], [E, 3], [C, 2] remove(pq, 5) results in
-        // pq: [A, 10], [D, 8], [B, 5].
-    }
-    public static <T> boolean search(Stack<T> st, T e)
-    {
-        throw new UnsupportedOperationException("Not supported yet.");
-        // Write the recursive static method search that searches for an element e in a stack st
-        // and returns true if it’s found or false otherwise. st should not change at the end of
-        // the method. You are not allowed to use any auxiliary data structures.
-        // Example. Given the stack st (top-to-bottom): 5, 7, 5, 3, 2. search(st, 3) returns
-        // true while search(st, 1) returns false.
-    }
-}
+
+	public static <T> void split(Queue<T> q, Queue<T> oq, Queue<T> eq) {
+		recsplit(q, oq, eq, q.length(), 1);	}
+	public static <T> void recsplit(Queue<T> q, Queue<T> oq, Queue<T> eq, int count, int pos) {
+		if (pos > count)
+			return;
+		T x = q.serve();
+		q.enqueue(x);
+		if (pos % 2 == 1) {
+			if (!oq.full()) {
+				oq.enqueue(x);	}
+		} else {
+			if (!eq.full()) 
+				eq.enqueue(x);
+		}
+		recsplit(q, oq, eq, count, pos + 1);
+
+	}
+
+	public static <T> void remove(LinkedPQ<T> pq, int p) {
+		LinkedPQ<T> tempPQ = new LinkedPQ<T>();
+		int count = pq.length();
+		for (int i = 0; i < count; i++) {
+			PQElement<T> element = pq.serve();
+			if (element.p >= p)
+				tempPQ.enqueue(element.data, element.p);
+		}
+		count = tempPQ.length();
+		for (int i = 0; i < count; i++) {
+			PQElement<T> element = tempPQ.serve();
+			pq.enqueue(element.data, element.p);	}
+	}
+	public static <T> boolean search(Stack<T> st, T e) {
+		if (st.empty())
+			return false;
+		T top = st.pop();
+		boolean found;
+
+		if (top.equals(e))
+		found = true;
+		else		found = search(st, e);
+		st.push(top);
+		return found;
+	}}
